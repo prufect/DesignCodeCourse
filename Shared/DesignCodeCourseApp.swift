c0d5fd1d240918381de844a4bb908ccdb38e71f6
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-07-29 19:23:10 -0700

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Prudhvi Gadiraju on 7/29/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
