063a35d03d44309f2cd69f7013006e80dad63343
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-07-29 20:55:26 -0700

    Create List

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,22 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Prudhvi Gadiraju on 7/29/20.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 5) { item in
+            CourseRow()
+        }.listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
