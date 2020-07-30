5c32629763da2488da9f32747bab769209a6c279
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-07-30 09:21:29 -0700

    ZIndex Fix Somehow

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -19,26 +19,48 @@ struct CoursesView: View {
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 160))], spacing: 16) {
                     ForEach(courses) { item in
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(height: 200)
-                            .onTapGesture { withAnimation(.spring()) {
-                                show.toggle()
-                                selectedItem = item
-                                isDisabled = true
-                            }}
-                            .disabled(isDisabled)
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture { withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
+                                }}
+                                .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
+
                     }
                 }
                 .padding()
                 .frame(maxWidth: .infinity)
             }
+            .zIndex(1)
             
             if selectedItem != nil {
-                ScrollView {
-                    CourseItem(course: selectedItem!)
-                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
-                        .frame(height: 300)
+                ZStack(alignment: .topTrailing) {
+                    VStack {
+                        ScrollView {
+                            CourseItem(course: selectedItem!)
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
+                                .frame(height: 300)
+                                
+                            VStack(spacing: 4.0) {
+                                ForEach(0 ..< 5) { item in
+                                    CourseRow()
+                                }
+                            }.padding()
+                        }
+                    }
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace, isSource: !show)
+                    .edgesIgnoringSafeArea(.all)
+                    
+                    CloseButton()
+                        .padding(.trailing, 16)
                         .onTapGesture { withAnimation(.spring()) {
                             show.toggle()
                             selectedItem = nil
@@ -46,20 +68,9 @@ struct CoursesView: View {
                                 isDisabled = false
                             }
                         }}
-                    VStack(spacing: 4.0) {
-                        ForEach(0 ..< 5) { item in
-                            CourseRow()
-                        }
-                    }.padding()
                 }
-                .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion: AnyTransition.opacity.animation(
-                            Animation.spring().delay(0.3)),
-                        removal: AnyTransition.opacity.animation(.spring())))
-                    
-                .edgesIgnoringSafeArea(.all)
+                .zIndex(2)
+
             }
         }
     }
