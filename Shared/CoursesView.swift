4c7af075e945053423ef0435956434aa9a3d1328
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-07-30 08:39:36 -0700

    Add Courses and Animate Them

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CoursesView: View {
     
     @State private var show: Bool = false
+    @State private var selectedItem: Course? = nil
+    @State private var isDisabled: Bool = false
     @Namespace var namespace
     
     var body: some View {
@@ -20,14 +22,27 @@ struct CoursesView: View {
                         CourseItem(course: item)
                             .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
                             .frame(width: 335, height: 250)
+                            .onTapGesture { withAnimation(.spring()) {
+                                show.toggle()
+                                selectedItem = item
+                                isDisabled = true
+                            }}
+                            .disabled(isDisabled)
                     }
                 }.frame(maxWidth: .infinity)
             }
-            if show {
+            if selectedItem != nil {
                 ScrollView {
-                    CourseItem(course: courses[0])
-                        .matchedGeometryEffect(id: courses[0].id, in: namespace)
+                    CourseItem(course: selectedItem!)
+                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
                         .frame(height: 300)
+                        .onTapGesture { withAnimation(.spring()) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                isDisabled = false
+                            }
+                        }}
                     VStack(spacing: 4.0) {
                         ForEach(0 ..< 5) { item in
                             CourseRow()
@@ -35,15 +50,16 @@ struct CoursesView: View {
                     }.padding()
                 }
                 .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion: AnyTransition.opacity.animation(
-                            Animation.spring().delay(0.3)),
-                        removal: AnyTransition.opacity.animation(.spring())))
+                .transition(.move(edge: .bottom))
+//                .transition(
+//                    .asymmetric(
+//                        insertion: AnyTransition.opacity.animation(
+//                            Animation.spring().delay(0.3)),
+//                        removal: AnyTransition.opacity.animation(.spring())))
                     
                 .edgesIgnoringSafeArea(.all)
             }
-        }.onTapGesture { withAnimation(.spring()) { show.toggle() }}
+        }
     }
 }
 
